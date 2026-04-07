aff6543a8a0d3aa7592c362328a5e48c04b415e5
Waseef Nayeem <haxerus.corporation@gmail.com>
2026-04-06 22:26:03 -0400

    refactor: merge client payload handlers into DuelNetworking for 1.21.1

--- src/main/java/com/haxerus/duelcraft/DuelcraftClient.java
+++ src/main/java/com/haxerus/duelcraft/DuelcraftClient.java
@@ -1,43 +1,23 @@
 package com.haxerus.duelcraft;
 
-import com.haxerus.duelcraft.client.ClientPayloadHandler;
-import com.haxerus.duelcraft.server.DuelEndPayload;
-import com.haxerus.duelcraft.server.DuelMessagePayload;
-import com.haxerus.duelcraft.server.DuelStartPayload;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import net.neoforged.neoforge.client.network.event.RegisterClientPayloadHandlersEvent;
 
-// This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = Duelcraft.MODID, dist = Dist.CLIENT)
-// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = Duelcraft.MODID, value = Dist.CLIENT)
 public class DuelcraftClient {
-    public DuelcraftClient(ModContainer container) {
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
+    public DuelcraftClient(IEventBus modBus, ModContainer container) {
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+        modBus.addListener(DuelcraftClient::onClientSetup);
     }
 
-    @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
+    private static void onClientSetup(FMLClientSetupEvent event) {
         Duelcraft.LOGGER.info("HELLO FROM CLIENT SETUP");
         Duelcraft.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-    }
-
-    @SubscribeEvent
-    static void onRegisterClientPayloads(RegisterClientPayloadHandlersEvent event) {
-        event.register(DuelStartPayload.TYPE, ClientPayloadHandler::handleStart);
-        event.register(DuelMessagePayload.TYPE, ClientPayloadHandler::handleMessage);
-        event.register(DuelEndPayload.TYPE, ClientPayloadHandler::handleEnd);
     }
 }
--- src/main/java/com/haxerus/duelcraft/server/DuelNetworking.java
+++ src/main/java/com/haxerus/duelcraft/server/DuelNetworking.java
@@ -1,5 +1,6 @@
 package com.haxerus.duelcraft.server;
 
+import com.haxerus.duelcraft.client.ClientPayloadHandler;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
@@ -7,9 +8,16 @@ public class DuelNetworking {
     public static void onRegisterPayloads(RegisterPayloadHandlersEvent event) {
         PayloadRegistrar registrar = event.registrar("1");
 
-        registrar.playToClient(DuelStartPayload.TYPE, DuelStartPayload.STREAM_CODEC);
-        registrar.playToClient(DuelMessagePayload.TYPE, DuelMessagePayload.STREAM_CODEC);
-        registrar.playToClient(DuelEndPayload.TYPE, DuelEndPayload.STREAM_CODEC);
-        registrar.playToServer(DuelResponsePayload.TYPE, DuelResponsePayload.STREAM_CODEC, ServerPayloadHandler::handleResponse);
+        // Server → Client (handlers run on client only)
+        registrar.playToClient(DuelStartPayload.TYPE, DuelStartPayload.STREAM_CODEC,
+                ClientPayloadHandler::handleStart);
+        registrar.playToClient(DuelMessagePayload.TYPE, DuelMessagePayload.STREAM_CODEC,
+                ClientPayloadHandler::handleMessage);
+        registrar.playToClient(DuelEndPayload.TYPE, DuelEndPayload.STREAM_CODEC,
+                ClientPayloadHandler::handleEnd);
+
+        // Client → Server
+        registrar.playToServer(DuelResponsePayload.TYPE, DuelResponsePayload.STREAM_CODEC,
+                ServerPayloadHandler::handleResponse);
     }
 }
